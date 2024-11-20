a7e7c2e8772f5e3fba648ad41fc1fb7bb5070cff
smol-ninja <shubhamy2015@gmail.com>
2024-11-20 21:25:42 +0000

    docs: add comments to create scripts

--- script/CreateMerkleLL.s.sol
+++ script/CreateMerkleLL.s.sol
@@ -9,19 +9,32 @@ import { MerkleBase, MerkleLL } from "../src/types/DataTypes.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
+/// @dev Creates a dummy campaign to airdrop tokens through Lockup Linear.
 contract CreateMerkleLL is BaseScript {
     /// @dev Deploy via Forge.
     function run() public virtual broadcast returns (ISablierMerkleLL merkleLL) {
-        // Prepare the constructor parameters.
         // TODO: Update address once deployed.
         ISablierMerkleFactory merkleFactory = ISablierMerkleFactory(0xF35aB407CF28012Ba57CAF5ee2f6d6E4420253bc);
 
+        // Prepare the constructor parameters.
         MerkleBase.ConstructorParams memory baseParams;
+
+        // The token to distribute through the campaign.
         baseParams.token = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+        // The campaign will expire in 30 days.
         baseParams.expiration = uint40(block.timestamp + 30 days);
+
+        // The admin of the campaign.
         baseParams.initialAdmin = 0x79Fb3e81aAc012c08501f41296CCC145a1E15844;
+
+        // A dummy IPFS CID where the campaign's metadata can be stored.
         baseParams.ipfsCID = "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR";
+
+        // A dummy Merkle root hash.
         baseParams.merkleRoot = 0x0000000000000000000000000000000000000000000000000000000000000000;
+
+        // A dummy name for the campaign.
         baseParams.name = "The Boys LL";
 
         // Deploy the MerkleLL contract.
