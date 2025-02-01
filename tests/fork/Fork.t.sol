f7243d397c18825dbf608c2b2c12716706acde1d
smol-ninja <shubhamy2015@gmail.com>
2025-02-01 23:17:21 +0200

    test: use mainnet address in fork testing

--- tests/fork/Fork.t.sol
+++ tests/fork/Fork.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.22 <0.9.0;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ISablierLockup } from "@sablier/lockup/src/interfaces/ISablierLockup.sol";
 import { Merkle } from "murky/src/Merkle.sol";
+import { ISablierMerkleFactory } from "src/interfaces/ISablierMerkleFactory.sol";
 
 import { Base_Test } from "../Base.t.sol";
 
@@ -29,12 +30,10 @@ abstract contract Fork_Test is Base_Test, Merkle {
 
     function setUp() public virtual override {
         // Fork Ethereum Mainnet at a specific block number.
-        vm.createSelectFork({ blockNumber: 21_745_237, urlOrAlias: "mainnet" });
+        vm.createSelectFork({ blockNumber: 21_719_244, urlOrAlias: "mainnet" });
 
-        // Set up the parent test contract.
-        Base_Test.setUp();
-
-        // Load the pre-deployed external dependencies.
+        // Load deployed addresses from Ethereum mainnet.
+        merkleFactory = ISablierMerkleFactory(0x71DD3Ca88E7564416E5C2E350090C12Bf8F6144a);
         lockup = ISablierLockup(0x7C01AA3783577E15fD7e272443D44B92d5b21056);
     }
 }
