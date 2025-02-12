ae31f295587e50a3a255086300286ba4845dbcbd
smol-ninja <shubhamy2015@gmail.com>
2025-02-12 15:23:19 +0000

    refactor: merkle campaign constructors (#54)
    * refactor: separate out parameters struct for Merkle Instant and Merkle Lockups
    refactor: refactor shape and camapign name as bytes32 in Merkle campaign constructor arguments
    
    * chore: contract inheritance
    
    * test: deploy factory locally for fork tests
    
    * refactor: create param struct for merkle contracts
    
    * refactor: rename CreateParams to ConstructorParams
    
    refactor: remove aggregateAmount and recipientCount from ConstructorParams
    
    * refactor: say params instead of constructorParams
    
    docs: add natspec for missing params
    
    ---------
    
    Co-authored-by: Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>

--- script/CreateMerkleInstant.s.sol
+++ script/CreateMerkleInstant.s.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.22 <0.9.0;
 
-import { ISablierMerkleFactory } from "../src/interfaces/ISablierMerkleFactory.sol";
-import { ISablierMerkleInstant } from "../src/interfaces/ISablierMerkleInstant.sol";
-import { MerkleBase } from "../src/types/DataTypes.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ISablierMerkleFactory } from "src/interfaces/ISablierMerkleFactory.sol";
+import { ISablierMerkleInstant } from "src/interfaces/ISablierMerkleInstant.sol";
+import { MerkleInstant } from "src/types/DataTypes.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
@@ -14,26 +15,21 @@ contract CreateMerkleInstant is BaseScript {
         ISablierMerkleFactory merkleFactory = ISablierMerkleFactory(0x71DD3Ca88E7564416E5C2E350090C12Bf8F6144a);
 
         // Prepare the constructor parameters.
-        MerkleBase.ConstructorParams memory baseParams;
-
-        // The campaign will expire in 30 days.
-        baseParams.expiration = uint40(block.timestamp + 30 days);
-
-        // The admin of the campaign.
-        baseParams.initialAdmin = 0x79Fb3e81aAc012c08501f41296CCC145a1E15844;
-
-        // Dummy values for the campaign name, IPFS CID, and the Merkle root hash.
-        baseParams.campaignName = "The Boys Instant";
-        baseParams.ipfsCID = "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR";
-        baseParams.merkleRoot = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        MerkleInstant.ConstructorParams memory params;
+        params.campaignName = "The Boys Instant";
+        params.expiration = uint40(block.timestamp + 30 days);
+        params.initialAdmin = 0x79Fb3e81aAc012c08501f41296CCC145a1E15844;
+        params.ipfsCID = "QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR";
+        params.merkleRoot = 0x0000000000000000000000000000000000000000000000000000000000000000;
+        params.token = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
         // The total amount to airdrop through the campaign.
-        uint256 campaignTotalAmount = 10_000e18;
+        uint256 aggregateAmount = 10_000e18;
 
         // The number of eligible users for the airdrop.
         uint256 recipientCount = 100;
 
         // Deploy the MerkleInstant contract.
-        merkleInstant = merkleFactory.createMerkleInstant(baseParams, campaignTotalAmount, recipientCount);
+        merkleInstant = merkleFactory.createMerkleInstant(params, aggregateAmount, recipientCount);
     }
 }
