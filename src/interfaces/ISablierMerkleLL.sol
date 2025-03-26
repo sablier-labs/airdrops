2ecf74cdf5e5246a37ce216b597cb58f0d4cf4f6
Paul Razvan Berg <prberg@proton.me>
2025-03-26 19:20:50 +0200

    docs: update NatSpec

--- src/interfaces/ISablierMerkleLL.sol
+++ src/interfaces/ISablierMerkleLL.sol
@@ -12,20 +12,20 @@ interface ISablierMerkleLL is ISablierMerkleLockup {
                                  CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    ///@notice Retrieves the cliff duration of the vesting Lockup stream, in seconds.
+    ///@notice Retrieves the cliff duration of the vesting stream, in seconds.
     function VESTING_CLIFF_DURATION() external view returns (uint40);
 
     /// @notice Retrieves the percentage of the claim amount due to be unlocked at the vesting cliff time, as a
     /// fixed-point number where 1e18 is 100%.
     function VESTING_CLIFF_UNLOCK_PERCENTAGE() external view returns (UD60x18);
 
-    /// @notice Retrieves the start time of the vesting Lockup stream. Zero is a sentinel value for `block.timestamp`.
+    /// @notice Retrieves the start time of the vesting stream. Zero is a sentinel value for `block.timestamp`.
     function VESTING_START_TIME() external view returns (uint40);
 
     /// @notice Retrieves the percentage of the claim amount due to be unlocked at the vesting start time, as a
     /// fixed-point number where 1e18 is 100%.
     function VESTING_START_UNLOCK_PERCENTAGE() external view returns (UD60x18);
 
-    /// @notice Retrieves the total duration of the vesting Lockup stream, in seconds.
+    /// @notice Retrieves the total duration of the vesting stream, in seconds.
     function VESTING_TOTAL_DURATION() external view returns (uint40);
 }
