65679cad63beeca3e48bed35fefae3a3389e0fe7
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-05-30 12:20:52 +0300

    test: unused modifiers

--- tests/utils/Modifiers.sol
+++ tests/utils/Modifiers.sol
@@ -112,10 +112,6 @@ abstract contract Modifiers is EvmUtilsBase {
         _;
     }
 
-    modifier whenFactoryAdminIsContract() {
-        _;
-    }
-
     modifier whenFeeRecipientContract() {
         _;
     }
@@ -209,10 +205,6 @@ abstract contract Modifiers is EvmUtilsBase {
     }
 
     modifier whenVestingStartTimeNotZero() {
-        _;
-    }
-
-    modifier whenWithdrawalAddressNotZero() {
         _;
     }
 }
