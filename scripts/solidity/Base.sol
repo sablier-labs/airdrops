2244046ad453308f146738d7fa98a4271f8c43ad
smol-ninja <shubhamy2015@gmail.com>
2025-06-13 11:44:30 +0100

    chore: remove redundant code

--- scripts/solidity/Base.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// solhint-disable code-complexity
-pragma solidity >=0.8.22 <0.9.0;
-
-import { BaseScript as EvmUtilsBaseScript } from "@sablier/evm-utils/src/tests/BaseScript.sol";
-
-abstract contract BaseScript is EvmUtilsBaseScript { }
